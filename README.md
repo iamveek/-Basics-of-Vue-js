928536337729504aabccda0e16ad1693c5caa94b
Muhammad Afaq Shahid Khan <97670374+iamveek@users.noreply.github.com>
2022-01-16 15:52:32 +0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# -Basics-of-Vue-js
-First code day in Vue.jjs
+# blog
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/components/Home.vue
@@ -0,0 +1,64 @@
+<template>
+
+<h1> Home screen </h1>
+<h1>Email {{email}} </h1>
+<h1>Phone {{phone}} </h1>
+<h1>Net Worth {{ netWorth()}} </h1>
+<h1> Name : {{getData().email}}</h1>
+<button v-on:click= "getEvent(' I clicked')" >Click me</button>
+<button v-on:dblclick= "getDClick('Doubled')">Click double </button>
+<h2 v-on:mousemove= "moveMouse()"> Console Mouse </h2>
+<button v-on:click="getCount()"> press</button>
+<h2>{{count}}</h2>
+ </template>
+
+<script> 
+
+export default {
+    name: "Home",
+   data(){
+     return {
+        email: 'afaqk3203@gmail.com',
+    phone: '0302',
+    netWorth: function(){
+     return "20 K";
+     
+    },
+count:0
+     }
+   },
+   methods: {
+     getData(){
+       return {
+         name: "Afaq",
+         email: " Azadi @gmail.com",
+
+       
+       }
+     },
+     getCount(){
+       this.count= this.count+1;
+     },
+     getEvent(data){
+       alert (data);
+     },
+     getDClick(data)
+     {
+       alert (data);
+     },
+     moveMouse()
+     {
+       console.warn("Mouse Move")
+
+     },
+   }
+}
+
+  </script>
+  <style scoped> 
+  h1{
+  color: orange
+  }
+  
+  
+  </style>
